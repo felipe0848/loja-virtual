969cfa49a3e5d838086130f7021cd9ca8a4b6b90
Felipe Soares <felipe.11.11@hotmail.com>
2023-09-04 16:08:43 -0400

    Feat: add design responsive in product page

--- capputeeno/src/components/ProductComponents/InfoProduct.tsx
+++ capputeeno/src/components/ProductComponents/InfoProduct.tsx
@@ -7,8 +7,10 @@ import ButtonBuy from "./ButtonBuy";
 
 const Container = styled.div`
     margin-top: 22px;
+
     display: flex;
-    gap: 32px;
+    flex-direction: column;
+    gap: 16px;
     border-radius: 4px;
     width: 100%;
 
@@ -18,15 +20,28 @@ const Container = styled.div`
     color: var(--textos-dark-primary, #41414d);
 
     img {
-        width: 50%;
-        max-width: 640px;
-        max-height: 580px;
+        width: 100%;
     }
     div {
         display: flex;
         flex-direction: column;
         justify-content: space-between;
         align-items: start;
+    }
+    button {
+        font-size: 14px;
+    }
+    @media (min-width: ${(props) => props.theme.tableBreakPoint}) {
+        flex-direction: row;
+        img {
+            width: 50%;
+            max-width: 640px;
+            max-height: 580px;
+            gap: 32px;
+        }
+        button {
+            font-size: 16px;
+        }
     }
 `;
 
@@ -50,7 +65,7 @@ const TextDiv = styled.div`
         font-size: 12px;
     }
     h4 {
-        margin-top: 58px;
+        margin-top: 24px;
         margin-bottom: 8px;
         color: var(--textos-dark-secondary, #737380);
         font-size: 16px;
@@ -58,8 +73,16 @@ const TextDiv = styled.div`
         text-transform: uppercase;
     }
     p:nth-child(3) {
-        font-size: 14px;
+        font-size: 12px;
         font-weight: 400;
+    }
+    @media (min-width: ${(props) => props.theme.tableBreakPoint}) {
+        h4 {
+            margin-top: 58px;
+        }
+        p:nth-child(3) {
+            font-size: 14px;
+        }
     }
 `;
 
